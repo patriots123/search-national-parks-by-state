ac91717b5b88468f5fd94a97413774761520680b
Will Shaughnssy <WillShaughnessy@MacBook-Pro.hsd1.ma.comcast.net>
2018-12-24 15:57:27 -0500

    updated CSS JS and HTML - 12/24

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
 'use strict';
 
-const apiKey = 'AIzaSyA_Qp0hfGAP38MmYnyRF-KIXEXwfK9_sMM'; 
-const searchURL = 'https://www.googleapis.com/youtube/v3/search';
-
-
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
 }
 
-function displayResults(responseJson) {
-  // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
-    $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
+function displayResults(responseJson,maxResults) {
+    console.log(responseJson);
+    $('#results-list').empty();
+    if (array === undefined || array.length == 0) {
+        $('#results-list').append(`No results for search citeria`);
+    } else {
+        for (let i = 0; i < responseJson.data.length; i++){
+          $('#results-list').append(
+            `<li><h3>${responseJson.data[i].name}</h3>
+            <p>${responseJson.data[i].description}</p>
+            <a href='${responseJson.data[i].url}'>Website for ${responseJson.data[i].name}</a>
+            </li>`
+          )};
+        $('.js-search-results-title').text(`First ${maxResults} Search Results in Specified State(s)`);
+    }
+    $('#results').removeClass('hidden');
 };
 
-function getYouTubeVideos(query, maxResults=10) {
-  const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+function getListOfParks(query, maxResults) {
+    const apiKey = 'R0RWdCtG6Q83qBvOuhpC0ApOqIK85wLkNMwyLT39'; 
+    const searchURL = 'https://api.nps.gov/api/v1/parks';
 
-  console.log(url);
+    const params = {
+        api_key: apiKey,
+        stateCode: query,
+        limit: maxResults,
+        sort: 'name',
+    };
+    
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
 
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-}
+    console.log(url);
+
+        fetch(url, params)
+        .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson,maxResults))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}   
 
 function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
-  });
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchTerm = $('#js-search-term').val();
+      const maxResults = $('#js-max-results').val();
+      getListOfParks(searchTerm, maxResults);
+    });
 }
 
 $(watchForm);
